e15b8cfe095b9b3a6ce887e415adbf6ee7c57565
rOreBurn Intern <roreburnintern@gmail.com>
2026-03-07 22:20:00 +0000

    Disable Turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Disable Turbopack for now - use webpack
+  turbopack: false,
 };
 
 export default nextConfig;
