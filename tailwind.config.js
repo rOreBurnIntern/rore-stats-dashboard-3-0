d879b2bc5bf656f041ffa187aef9d52b997fabda
rOreBurn Intern <roreburnintern@gmail.com>
2026-03-07 22:33:52 +0000

    Fix Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./app.js",
-    "./node_modules/flowbite/**/*.js",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: 'class',
-  theme: {},
-  plugins: [
-    require('daisyui'),
-    require('flowbite/plugin')
-  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
